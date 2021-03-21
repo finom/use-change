6493814d8b607e3ed5f9374166e2180af5da3ad9
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-03-21 18:21:40 +0200

    fix: Update types

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ export type Key<SLICE, KEY> = keyof SLICE & string & KEY;
 export type ReturnTuple<T> = [T, (value: T | ((value: T) => T)) => void];
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Handler<SLICE = any, KEY = any> = (value: SLICE[Key<SLICE, KEY>]) => void;
+export type Handler<SLICE = any, KEY = any> = (value: SLICE[Key<SLICE, KEY>]) => any;
 
 export interface Selector<STORE, SLICE> {
   (store: STORE): SliceRecord<SLICE>;
