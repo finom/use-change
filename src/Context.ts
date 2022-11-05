77cfcb1d3f24611d4aea03567704778fd92d0716
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2022-11-05 18:25:18 +0100

    fix: Minor

--- src/Context.ts
+++ src/Context.ts
@@ -1,9 +1,9 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 // eslint-disable-next-line import/no-mutable-exports
 export let Context = createContext<unknown>({});
 
 export function defineContext<T>(ctx: T) {
-  Context = ctx as React.Context<unknown>;
+  Context = ctx as unknown as typeof Context;
   return Context;
 }
