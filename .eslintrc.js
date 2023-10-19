952052151022d9e71624c8d47051638b0b893099
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2023-10-19 17:22:50 +0200

    chore: Create tsconfig for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,17 +15,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
-  /*
-  settings: {
-    'import/resolver': {
-      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
-      node: {},
-      webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.js'),
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: './tsconfig.eslint.json',
       },
     },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-  }, */
+  ],
 };
